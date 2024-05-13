a5e8abd8b5bd433f48673468db2edeb2b860f766
sekitomodas <s.tomoya0319@icloud.com>
2024-05-13 15:33:44 +0900

    Employee

---
+++ src/main/java/com/example/domain/Employee.java
@@ -0,0 +1,102 @@
+package com.example.domain;
+public class Employee {
+
+    private Integer ID;/** Id */
+    private String name; /** 名前 */
+    private String image;/** 画像 */
+    private String gender;/** 性別 */
+    private java.util.Date hireDate;/** 入社日 */
+    private String mailAddress;/** メールアドレス */
+    private String zipCode;/** 郵便番号 */
+    private String address;/** 住所 */
+    private String telephone;/** 電話番号 */
+    private Integer salary;/** 給料 */
+    private String characteristics;/** 特徴 */
+    private Integer dependentsCount;/** 扶養人数 */
+
+
+    
+    @Override
+    public String toString() {
+        return "Employee [ID=" + ID + ", name=" + name + ", image=" + image + ", gender=" + gender + ", hireDate="
+                + hireDate + ", mailAddress=" + mailAddress + ", zipCode=" + zipCode + ", address=" + address
+                + ", telephone=" + telephone + ", salary=" + salary + ", characteristics=" + characteristics
+                + ", dependentsCount=" + dependentsCount + "]";
+    }
+    public Integer getID() {
+        return ID;
+    }
+    public void setID(Integer iD) {
+        ID = iD;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public java.util.Date getHireDate() {
+        return hireDate;
+    }
+    public void setHireDate(java.util.Date hireDate) {
+        this.hireDate = hireDate;
+    }
+    public String getMailAddress() {
+        return mailAddress;
+    }
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+    public String getZipCode() {
+        return zipCode;
+    }
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    public Integer getSalary() {
+        return salary;
+    }
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+    public String getCharacteristics() {
+        return characteristics;
+    }
+    public void setCharacteristics(String characteristics) {
+        this.characteristics = characteristics;
+    }
+    public Integer getDependentsCount() {
+        return dependentsCount;
+    }
+    public void setDependentsCount(Integer dependentsCount) {
+        this.dependentsCount = dependentsCount;
+    }
+
+
+    
+
+}
